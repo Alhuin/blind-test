be40a0ff21ab51dcddb0983eaf0750e172e3e0d7
Alhuin <julien.janinr@protonmail.com>
2020-03-30 11:58:32 +0200

    removed next

--- src/Components/Controls.js
+++ src/Components/Controls.js
@@ -2,11 +2,10 @@ import React, { Component } from 'react'
 
 class Controls extends Component {
   render() {
-    const { launch, next } = this.props;
+    const { launch } = this.props;
     return (
       <div>
         <img id={'launch'} src={require('../assets/icons/play.png')} onClick={launch} alt={'play'} className={'control'} style={{display: (this.props.admin) ? 'block' : 'none'}}/>
-        {/*<img src={require('../assets/icons/next.png')} onClick={next} alt={'next'} className={'control'}/>*/}
       </div>
     )
   }
